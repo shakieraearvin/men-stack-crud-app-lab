58728b0bb252c68ca918592080a49cbad5e481c7
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-17 17:27:15 -0500

    added express and views

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});

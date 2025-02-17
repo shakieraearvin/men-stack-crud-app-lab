364a20a28382c03af9d687b6d0619977930691b3
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-17 18:00:17 -0500

    added new cheeses ejs

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
 
+// GET /fruits/new
+app.get("/cheeses/new", (req, res) => {
+    res.render("fruits/new.ejs");
+  });
+   
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');

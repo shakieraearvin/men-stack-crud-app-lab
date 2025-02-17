bd422d19936bd17883cb0274b5c3e5a24fc4eb44
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-17 18:39:00 -0500

    added links to files

--- server.js
+++ server.js
@@ -23,23 +23,36 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
 
-// GET /fruits/new
+  app.get("/cheeses", async (req, res) => {
+    const allCheeses = await Cheese.find();
+    console.log(allCheeses);
+    res.render("cheeses/index.ejs", { cheeses: allCheeses });
+  });
+  
 app.get("/cheeses/new", (req, res) => {
     res.render("fruits/new.ejs");
   });
 
 
 
-app.post("/fruits", async (req, res) => {
-    if (req.body.isReadyToEat === "on") {
-      req.body.isReadyToEat = true;
+app.post("/cheeses", async (req, res) => {
+    if (req.body.isStinky === "on") {
+      req.body.isStinky = true;
     } else {
-      req.body.isReadyToEat = false;
+      req.body.isStinky = false;
     }
-    await Fruit.create(req.body);
-    res.redirect("/fruits/new");
+    await Cheese.create(req.body);
+    res.redirect("/cheeses");
   });
   
+
+
+
+
+
+
+
+
 
 
 app.listen(3000, () => {

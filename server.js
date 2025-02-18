966df9b5f8dc4020c42c1ea4ddf414431ccffbe3
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-18 16:50:51 -0500

    finished lab

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const dotenv = require("dotenv"); // require package
 dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
 const mongoose = require("mongoose"); // require package
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
-// log connection status to terminal on start
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+const Cheese = require("./models/cheese.js");
 
 // middleware 
 app.use(express.urlencoded({ extended: false }));
-
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 
 
@@ -33,8 +37,13 @@ app.get("/cheeses/new", (req, res) => {
     res.render("fruits/new.ejs");
   });
 
+app.get("/cheeses/:id", async (req, res) => {
+    const foundCheese = await Cheese.findById(req.params.cheeseId);
+    res.render("cheeses/show.ejs", { cheese: foundCheese });
+  });
 
 
+// POST
 app.post("/cheeses", async (req, res) => {
     if (req.body.isStinky === "on") {
       req.body.isStinky = true;
@@ -45,13 +54,26 @@ app.post("/cheeses", async (req, res) => {
     res.redirect("/cheeses");
   });
   
+app.delete("/cheeses/:cheeseId", async (req, res) => {
+    await Cheese.findByIdAndDelete(req.params.cheeseId);
+    res.redirect("/cheeses");
+  });
 
+app.get("/cheeses/:cheeseId/edit", async (req, res) => {
+    const foundCheese = await Cheese.findById(req.params.cheeseId);
+    res.render("cheeses/edit.ejs", { cheese: foundCheese });
+  });
 
+app.put("/cheeses/:cheeseId", async (req, res) => {
+    if (req.body.isStinky === "on") {
+      req.body.isStinky = true;
+    } else {
+      req.body.isStinky = false;
+    }
+    await Cheese.findByIdAndUpdate(req.params.cheeseId, req.body);
 
-
-
-
-
+    res.redirect(`/cheeses/${req.params.cheeseId}`);
+});
 
 
 
